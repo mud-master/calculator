e419435490676868862d77736bc5f2f3cf4b87b4
mud-master <ctvonkleeck@gmail.com>
2024-06-07 08:12:43 -0400

    Add MDAS capabilities with multiple digit numbers

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, operate) {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+
   if (operate == "+") {
     return add(num1, num2);
   } else if (operate == "-") {
@@ -34,56 +37,118 @@ function operate(num1, num2, operate) {
   }
 }
 
-function displayEquation(num1, num2, operator) {
-    if (!equationFinished) {
-        if (num1 == null) {
-            return '';
-        } else if (operator == null) {
-            return num1;
-        } else if (num2 == null) {
-            return num1 + ' ' + operator;
-        }
-        else {
-            return num1 + ' ' + operator + ' ' + num2;
-        }
-    }
 
-    else {
-        return operate(parseInt(num1), parseInt(num2), operator);
-    }
+function calculate(numsArr) {
+  while (numsArr.length > 1) {
+    let firstMult = numsArr.indexOf("*");
+    let firstDiv = numsArr.indexOf("/");
+    let firstAdd = numsArr.indexOf("+");
+    let firstSub = numsArr.indexOf("-");
 
+    if (firstMult != -1) {
+      let result = operate(numsArr[firstMult - 1], numsArr[firstMult + 1], "*");
+      numsArr.splice(firstMult - 1, 3, result);
+    } else if (firstDiv != -1) {
+      let result = operate(numsArr[firstDiv - 1], numsArr[firstDiv + 1], "/");
+      numsArr.splice(
+        firstDiv - 1,
+        3,
+        operate(numsArr[firstDiv - 1], numsArr[firstDiv + 1], "/")
+      );
+    } else if (firstAdd != -1) {
+      let result = operate(numsArr[firstAdd - 1], numsArr[firstAdd + 1], "+");
+      numsArr.splice(
+        firstAdd - 1,
+        3,
+        operate(numsArr[firstAdd - 1], numsArr[firstAdd + 1], "+")
+      );
+    } else if (firstSub != -1) {
+      let result = operate(numsArr[firstSub - 1], numsArr[firstSub + 1], "-");
+      numsArr.splice(
+        firstSub - 1,
+        3,
+        operate(numsArr[firstSub - 1], numsArr[firstSub + 1], "-")
+      );
+    }
+  }
+  return numsArr[0];
 }
+
 
 // Finish the calculator GUI
 const calc = document.querySelector("#calc"); // The main div in index.html
 const input = document.querySelector("#input"); // User types input in this div
+
+// Let's change this up a bit...
+let numsArr = [];
+let validOperators = new Set(["+", "-", "/", "*"]);
+
+document.addEventListener("keydown", function (event) {
+  const output = document.querySelector("#output"); // output box on top of page
+  console.log('is even: ' + numsArr.length % 2 == 0);
+
+  if (event.key >= 0 && event.key <= 9 && numsArr.length % 2 == 0) {
+    numsArr.push(event.key);
+    output.textContent = numsArr.join(' ');
+  } else if (event.key >= 0 && event.key <= 9) {
+    numsArr[numsArr.length - 1] = numsArr[numsArr.length - 1] + event.key;
+    output.textContent = numsArr.join(' ');
+  } else if (validOperators.has(event.key)) {
+    numsArr.push(event.key);
+    output.textContent = numsArr.join(' ');
+  } else if (event.key == "Enter" && numsArr.length % 2 == 1) {
+    console.log('finished');
+    output.textContent = calculate(numsArr);
+    numsArr = [];
+  }
+  console.log('e' + numsArr);
+
+});
+
+/*
+
+function displayEquation(num1, num2, operator) {
+  if (!equationFinished) {
+    if (num1 == null) {
+      return "";
+    } else if (operator == null) {
+      return num1;
+    } else if (num2 == null) {
+      return num1 + " " + operator;
+    } else {
+      return num1 + " " + operator + " " + num2;
+    }
+  } else {
+    return operate(parseInt(num1), parseInt(num2), operator);
+  }
+}
 
 // Let's do some math!
 let num1 = null;
 let num2 = null;
 let operator = null;
 let equationFinished = false;
-let validOperators = new Set(["+", "-", "/", "*"]);
+//let validOperators = new Set(["+", "-", "/", "*"]);
 
 document.addEventListener("keydown", function (event) {
   const output = document.querySelector("#output"); // output box on top of page
 
-  if (event.key >= 0 && event.key <= 9 
-    && operator == null) {
-    num1 = (num1 == null ? event.key : num1 + event.key);
+  if (event.key >= 0 && event.key <= 9 && operator == null) {
+    num1 = num1 == null ? event.key : num1 + event.key;
   } else if (
     num1 != null &&
     operator == null &&
     validOperators.has(event.key)
   ) {
     operator = event.key;
+  } else if (operator != null && event.key >= 0 && event.key <= 9) {
+    num2 = num2 == null ? event.key : num2 + event.key;
   } else if (
+    num1 != null &&
     operator != null &&
-    event.key >= 0 &&
-    event.key <= 9
+    num2 != null &&
+    event.key == "Enter"
   ) {
-    num2 = (num2 == null ? event.key : num2 + event.key);
-  } else if (num1 != null && operator != null && num2 != null && event.key == 'Enter') {
     equationFinished = true;
   }
 
@@ -95,3 +160,4 @@ document.addEventListener("keydown", function (event) {
     equationFinished = false;
   }
 });
+*/
